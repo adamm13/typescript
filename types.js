e4b7c52a1ec6262433764d41db83c7cb4ef2630f
Adam Mohammed <13.adamm@gmail.com>
2021-05-12 11:51:52 -0400

    more type practice

--- types.js
+++ types.js
@@ -2,3 +2,31 @@
 var n = 1;
 //types are important during compile-time
 n = 'rob';
+//be more strict with types
+//any defeats the purpose of types.
+// Basic Types
+// Boolean
+// Number
+// String
+// Array
+// Enum
+// Any 
+// Void 
+var isWinter = false;
+isWinter = 123;
+var count = 5;
+var name = "bran";
+//array is a vector can contain one type of variable
+var names = ["Jon", "Rick", "Bob"]; //any array type allows mutiple types
+var Starks;
+(function (Starks) {
+    Starks[Starks["jon"] = 0] = "jon";
+    Starks[Starks["bob"] = 1] = "bob";
+    Starks[Starks["edgar"] = 2] = "edgar";
+    Starks[Starks["catlyn"] = 3] = "catlyn";
+})(Starks || (Starks = {}));
+; //set of strings/ keys 
+var cat = Starks.catlyn;
+function getName() {
+    console.log("winter is coming");
+}
--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 // types defined by colon
 
-let n : any = 1;
+var n : any = 1;
 
 //types are important during compile-time
 
@@ -8,3 +8,33 @@ n = 'rob';
 
 //be more strict with types
 //any defeats the purpose of types.
+
+
+// Basic Types
+
+// Boolean
+// Number
+// String
+// Array
+// Enum
+// Any 
+// Void 
+
+var isWinter: Boolean = false
+
+isWinter = 123;
+
+var count : Number = 5;
+var name : String = "bran";
+
+//array is a vector can contain one type of variable
+
+var names : string[] = ["Jon", "Rick", "Bob"]  //any array type allows mutiple types
+
+enum Starks {jon, bob, edgar, catlyn}; //set of strings/ keys 
+
+var cat : Starks = Starks.catlyn;
+
+function getName() : void{
+  console.log("winter is coming")
+}
